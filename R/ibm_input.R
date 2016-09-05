6cb62c10e1cae36f3ecf87c8a06a77dc3f441b7a
Aranka-S <aranka_steyaert@hotmail.com>
2016-09-05 16:14:40 +0200

    Functie die de file patches.txt uitschrijft toegevoegd

--- R/ibm_input.R
+++ R/ibm_input.R
@@ -1,4 +1,12 @@
-ibm_input <- funtion(){
-  
-  
+outputPatch<-function(seed=420,mapsize=1000,dist_min=200,areaM=2.5,areaSD=0.8,Npatch=6,disp=500, plotG=FALSE, output.patch = "patches.txt")
+{
+  library(MetaLandSim)
+  library(dplyr)
+  set.seed(seed)
+  patches.tmp <- rland.graph(mapsize = mapsize, dist_m = dist_min, 
+                             areaM = areaM, areaSD = areaSD, Npatch = Npatch, 
+                             disp = disp, plotG = plotG)
+  patches.out <- patches.tmp$nodes.characteristics %>%
+    select(ID, areas)
+  write.csv(patches.out, file=output.patch, eol=",\n", row.names = FALSE, col.names = FALSE)
 }
