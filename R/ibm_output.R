27b515af90017a5cf8da4ba1685496d2575e2053
Aranka-S <aranka_steyaert@hotmail.com>
2016-09-06 10:39:20 +0200

    Een werkende functie voor de evolutionaire output + toevoegingen aan gitignore

---
+++ R/ibm_output.R
@@ -0,0 +1,57 @@
+EvolutionOutput <- function(tmax = 1000, nPop = 6, oneLoop = F, run = 0, java.out = "Evolution.txt", carying.cap = c(6,8,9,9,17,10)){
+  opar <- par()
+  library(colorRamps)
+  library(grDevices)
+  if(oneLoop){
+    x <- read.csv(java.out)
+  }else{
+    u <- read.csv(java.out)
+    x <- u[u[,"run"]==run,]
+  }
+  maxAge <- max(x[,"age"])
+  K <- rep(carying.cap[1],tmax)
+  for(i in 2:nPop){
+    K <- rbind(K, rep(carying.cap[i],tmax))
+  }
+  PerPatch <- matrix(ncol=nPop,nrow=tmax)
+  PerAge <- matrix(ncol=maxAge,nrow=tmax)
+  PerPatchSex <- list("patch1" = matrix(ncol=2,nrow=tmax),"patch2" = matrix(ncol=2,nrow=tmax),"patch3" = matrix(ncol=2,nrow=tmax),
+                      "patch4" = matrix(ncol=2,nrow=tmax),"patch5" = matrix(ncol=2,nrow=tmax),"patch6" = matrix(ncol=2,nrow=tmax))
+  for(i in 0:(tmax-1)){
+    y<-x[x[,"timestep"]==i,]
+    for(j in 0:(nPop-1)){
+      PerPatch[i+1,j+1]<-sum(y[,"patch"]==j)
+      f<-y[y[,"patch"]==j,]
+      PerPatchSex[[(j+1)]][i+1,1] <- sum(f[,"sex"]=="vrouw")
+      PerPatchSex[[(j+1)]][i+1,2] <- sum(f[,"sex"]=="man")
+    }
+    for(k in 1:maxAge){
+      PerAge[i+1,k]<-sum(y[,"age"]==k)
+    }
+  }
+  plot(PerPatch[,1],type="l",col=1, xlim = c(0,t), main="Evolution of population size per patch")
+  l1 <- "Pop1"
+  for(i in 2:nPop){
+    lines(PerPatch[,i],col=i)
+    l1 <- c(l1,paste0("Pop",i))
+  }
+  legend("top", legend = l1, fill = c(1:nPop), cex=0.8)
+  par(xpd=T, mar=par()$mar+c(0,0,0,4))
+  barplot(t(PerPatch), col=primary.colors(6), main = "Total number of individuals per patch, per year")
+  legend((t+21),5*nPop,legend=l1, cex=0.8, fill=primary.colors(nPop))
+  barplot(t(PerAge), col=primary.colors(maxAge), main = "Age division per year, sum of all patches")
+  legend((t+21),5*nPop,legend=c(1:maxAge),cex=0.8, fill=primary.colors(maxAge))
+  par(opar)
+  par(mfrow=c(nPop/2,2))
+  for(i in 1:nPop){
+    barplot(t(PerPatchSex[[i]]), col=rainbow(2), main = paste("Patch",as.character(i)))
+    lines(K[(i),])
+  }
+  legend(0,25,legend=c("vrouw","man"),cex=0.8,fill=rainbow(2), bty="n")
+  par(mfrow=c(1,1))
+  par(opar) 
+}
+
+extinctionOutput <- function(){
+  
+}
